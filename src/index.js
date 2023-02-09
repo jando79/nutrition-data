c558b385659c3bd9b5a9f31fb8f08b194921f9d5
David Jandron <jandron1211@gmail.com>
2023-02-08 16:29:50 -0800

    ADD css and index.html
    Co-Authored-By: Asia Kaplanyan <a.kaplanyan@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import NutritionData from './js/nutrition-data.js';
 
 // Business Logic
 
-function getData (cuisine) {
+function getData(cuisine) {
   NutritionData.getData(cuisine)
-  .then(function(response) {
-    if (response) {
-      printElements(response, cuisine);
-    } else {
-      printError(response, cuisine);
-    }
-  });
+    .then(function (response) {
+      if (response) {
+        printElements(response, cuisine);
+      } else {
+        printError(response, cuisine);
+      }
+    });
 }
 
 
@@ -24,13 +24,40 @@ function printError(request, search) {
 }
 
 function printElements(response) {
-  document.querySelector('#showResult').innerText = response;
+  console.log(response.results);
+  let id = "";
+  let title = "";
+  let image = "";
+  const element = document.getElementById("div1");
+  for (let x in response.results) {
+
+    id = response.results[x].id;
+    addP(id);
+
+    title = response.results[x].title;
+    addP(title);
+
+    image = response.results[x].image;
+    let img1 = document.createElement("img");
+    img1.src = image;
+    element.appendChild(img1);
+    addP("");
+  }
+}
+
+function addP(text) {
+  const element = document.getElementById("div1");
+  const para = document.createElement("p");
+  const node = document.createTextNode(text);
+  para.appendChild(node);
+  element.appendChild(para);
 }
 
 
 function handleFormSubmission(event) {
   event.preventDefault();
   let cuisine = document.querySelector("input[name='cuisine']:checked").value;
+  document.getElementById("div1").innerHTML = "";
   getData(cuisine);
 }
 
